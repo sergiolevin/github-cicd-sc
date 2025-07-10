9b8650416e28f47425a9dbfa58eaa6a864a114a2
sergiolevin <sergiolevin>
2025-07-10 11:14:21 -0300

    Delete Customers; TaskID: Delete Customers

--- dbo/Tables/Creation-Scripts/Customer.sql
+++
@@ -1,9 +0,0 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Customer](
-	[Id] [nchar](10) COLLATE Modern_Spanish_CI_AS NULL,
-	[Name] [nchar](10) COLLATE Modern_Spanish_CI_AS NULL
-)
-GO
