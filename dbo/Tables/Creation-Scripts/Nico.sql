231bf6fa0947e12bce7f1eccb87ada9b93acef3f
sergiolevin <sergiolevin>
2025-07-10 14:02:48 -0300

    Task123; TaskID: Task123

---
+++ dbo/Tables/Creation-Scripts/Nico.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Nico](
+	[UserName] [nchar](10) COLLATE Modern_Spanish_CI_AS NULL,
+	[Address] [nchar](10) COLLATE Modern_Spanish_CI_AS NULL
+)
+GO
